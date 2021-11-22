8ee29ba2dfc112ea96b0b1695a2c8ae165a6bcf3
S3RV3R3STA <94179757+S3RV3R3STA@users.noreply.github.com>
2021-11-22 19:51:49 +0400

    1-st Narek's homework

--- narektest.php
+++ narektest.php
@@ -1,1 +1,55 @@
-//coding...
+
+<!DOCTYPE HTML>  
+<html>
+<head>
+<style>
+.error {color: #FF0000;}
+</style>
+</head>
+<body>  
+
+<?php
+// define variables and set to empty values
+$emailErr = "";
+$email = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+ 
+  if (empty($_POST["email"])) {
+    $emailErr = "Email is required";
+  } else {
+    $email = test_input($_POST["email"]);
+    if (!preg_match('/@gmail.com/',$email)) {
+      $emailErr = "Invalid email format";
+    }
+  }
+   
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+?>
+
+<h2>PHP Form Validation Example</h2>
+
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
+
+  E-mail: <input type="text" name="email">
+  <span class="error">* <?php echo $emailErr;?></span>
+  <br><br>
+  <input type="submit" name="submit" value="Submit">  
+</form>
+
+<?php
+echo "<h2>Your Input:</h2>";
+
+echo $email;
+
+?>
+
+</body>
+</html>
